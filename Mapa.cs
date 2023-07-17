4755215acf75bb22c2728ea45463c9c3b079cc45
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 13:43:22 +0100

    Add files via upload

---
+++ Mapa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioFunção
+{
+    public class Mapa
+    {
+        public static void CriarMapa(int l, int c, int[,] mapa)
+        {
+            for(int i = 0; i < l; i++)
+            {
+                for(int j = 0; j < c; j++)
+                {
+                    mapa[i, j] = 0;
+                }
+            }
+        }
+
+        public static void EscreverMapa(int l, int c, int[,] mapa)
+        {
+            for(int i =0; i < l; i++)
+            {
+                for(int j = 0; j < c; j++)
+                {
+                    Console.Write(mapa[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using ExercícioFunção;
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] m = new int[4, 4];
+        Mapa.CriarMapa(4, 4, m);
+        Mapa.EscreverMapa(4, 4, m);
+    }
+}
